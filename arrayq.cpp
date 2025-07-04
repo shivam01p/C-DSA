2d123626c6ffccfe60c69244c5ab517879f171af
shivam01p <shivamparmar549@gmail.com>
2025-07-05 02:02:28 +0530

    sub array que1 solved

--- arrayq.cpp
+++ arrayq.cpp
@@ -123,5 +123,22 @@ int main(){
 
     // insArray(a, sza, b, szb);
 
+//todo: Subarray (kadane's algorithm)
+
+    int n = 5;
+    int a[] = {1, 2, 3, 4, 5};
+
+    for(int st = 0; st < n; st++){
+
+        for(int end = st; end < n; end++){
+
+            for(int i = st; i <= end; i++){
+                cout << a[i];
+            }
+            cout << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
